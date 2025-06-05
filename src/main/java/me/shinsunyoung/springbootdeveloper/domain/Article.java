69f94000a28f9746a872aff0b4240958890e8b19
Juglee <jason0101084@gmail.com>
2025-06-06 01:36:44 +0900

    refactor: 06장 블로그 기획하고 API 만들기 - Article 애너테이션 추가

--- src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
+++ src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -1,9 +1,14 @@
 package me.shinsunyoung.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
     @Id // id 필드를 기본키로 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
@@ -20,22 +25,5 @@ public class Article {
     public Article(String title, String content) {
         this.title = title;
         this.content = content;
-    }
-
-    protected Article() { // 기본 생성자
-
-    }
-
-    //게터
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
