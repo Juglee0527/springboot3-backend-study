99a0d4bd18914a711d160186c371d1317c217c42
Juglee <jason0101084@gmail.com>
2025-06-07 01:54:02 +0900

    feat: 06장 블로그 기획하고 API 만들기 - BlogApiControllerTest 생성

---
+++ src/test/java/me/shinsunyoung/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -0,0 +1,35 @@
+package me.shinsunyoung.springbootdeveloper.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.shinsunyoung.springbootdeveloper.repository.BlogRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest // 테스트용 애플리케이션 컨텍스트
+@AutoConfigureMockMvc // MockMvc 생성 및 자동 구성
+class BlogApiControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper; // 직렬화, 역직렬화를 위한 클래스
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    BlogRepository blogRepository;
+
+    @BeforeEach // 테스트 실행 전 실행하는 메서드
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        blogRepository.deleteAll();
+    }
+}
